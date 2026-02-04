77d4c5816dbf82c1e63abc990f957563e9793c71
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2026-02-04 15:52:34 +0530

    updating interview views.py file

--- interviews/views.py
+++ interviews/views.py
@@ -54,7 +54,7 @@ class InterviewViewSet(viewsets.ModelViewSet):
         return queryset
     
     def perform_create(self, serializer):
-        serializer.save(created_by=self.context['request'].user)
+        serializer.save(created_by_id=1)
     
     def perform_update(self, serializer):
         serializer.save(updated_by=self.context['request'].user)
